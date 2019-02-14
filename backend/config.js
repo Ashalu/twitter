0ed13e00111120d8510134a77d0ab9fc4556f360
Ashalu <developer.aashish02@gmail.com>
2019-02-14 15:21:34 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+var appsettings = {
+    consumerkey: 'gscBCilVBZbnYWUVbPr2tbi7I',
+    consumersecret: 'NSqvW8o7CrueEziYnTbq2xvg5isuVUXt8M2OuSvRIBJ61RG9sJ',
+    bearertoken: ''
+};
+
+module.exports = appsettings;
